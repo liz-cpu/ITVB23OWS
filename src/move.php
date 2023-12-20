536ec55bc03e91393f04340140060940308a0285
liz-cpu <40073038+liz-cpu@users.noreply.github.com>
2023-12-20 21:54:53 +0100

    [Fix] Fix SonarQube issues in move.php

--- src/move.php
+++ src/move.php
@@ -20,9 +20,9 @@ if (!isset($board[$from])) {
     $_SESSION['error'] = "Queen bee is not played";
 } else {
     $tile = array_pop($board[$from]);
-    if (!hasNeighBour($to, $board))
+    if (!hasNeighBour($to, $board)) {
         $_SESSION['error'] = "Move would split hive";
-    else {
+    } else {
         $all = array_keys($board);
         $queue = [array_shift($all)];
         while ($queue) {
@@ -40,11 +40,14 @@ if (!isset($board[$from])) {
         if ($all) {
             $_SESSION['error'] = "Move would split hive";
         } else {
-            if ($from == $to) $_SESSION['error'] = 'Tile must move';
-            elseif (isset($board[$to]) && $tile[1] != "B") $_SESSION['error'] = 'Tile not empty';
-            elseif ($tile[1] == "Q" || $tile[1] == "B") {
-                if (!slide($board, $from, $to))
+            if ($from == $to) {
+                $_SESSION['error'] = 'Tile must move';
+            } elseif (isset($board[$to]) && $tile[1] != "B") {
+                $_SESSION['error'] = 'Tile not empty';
+            } elseif ($tile[1] == "Q" || $tile[1] == "B") {
+                if (!slide($board, $from, $to)) {
                     $_SESSION['error'] = 'Tile must slide';
+                }
             }
         }
     }
@@ -61,7 +64,7 @@ if (!isset($board[$from])) {
             $board[$to] = [$tile];
         }
         $_SESSION['player'] = 1 - $_SESSION['player'];
-        $db = include 'database.php';
+        $db = include_once 'database.php';
         $stmt = $db->prepare('insert into moves (game_id, type, move_from, move_to, previous_id, state) values (?, "move", ?, ?, ?, ?)');
         $state = getState();
         $stmt->bind_param('issis', $_SESSION['game_id'], $from, $to, $_SESSION['last_move'], $state);
